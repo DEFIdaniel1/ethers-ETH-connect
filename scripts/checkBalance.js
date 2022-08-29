4a8d989f3e10f0d43c42a8936352211c2a02e377
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-08-29 14:59:41 -0600

    ethers dai contract added for status checks

--- scripts/checkBalance.js
+++ scripts/checkBalance.js
@@ -1,22 +1,15 @@
-const { ethers } = require("ethers");
-require("dotenv").config();
+const { ethers } = require('ethers')
+require('dotenv').config()
 
-const ALCHEMY_ETH_API = process.env.ALCHEMY_ETH_API;
-const INFURA_ETH_URL = process.env.INFURA_ETH_URL;
-const bigAddress = "0x1AaddcF6749E6a56b1C704cD30221dF00a08659C";
+const ALCHEMY_ETH_API = process.env.ALCHEMY_ETH_API
+const bigAddress = '0x1AaddcF6749E6a56b1C704cD30221dF00a08659C'
 
 const provider = new ethers.providers.JsonRpcProvider(
-  `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ETH_API}`
-);
-// const provider2 = new ethers.providers.WebSocketProvider(INFURA_ETH_URL)
+    `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ETH_API}`
+)
 
 const getBalance = async () => {
-  const balance = await provider.getBalance(bigAddress);
-  const connectInfo = await provider.connection;
-  console.log(
-    `ETH Balance of ${bigAddress} is ${ethers.utils.formatEther(balance)} ETH.`
-  );
-  console.log(connectInfo);
-  console.log(INFURA_ETH_URL);
-};
-getBalance();
+    const balance = await provider.getBalance(bigAddress)
+    console.log(`ETH Balance of ${bigAddress} is ${ethers.utils.formatEther(balance)} ETH.`)
+}
+getBalance()
---
+++ scripts/checkContract.js
@@ -0,0 +1,34 @@
+const { ethers } = require('ethers')
+require('dotenv').config()
+const ALCHEMY_ETH_API = process.env.ALCHEMY_ETH_API
+
+const provider = new ethers.providers.JsonRpcProvider(
+    `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ETH_API}`
+)
+
+const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+// 2 methods to get the contract ABI - one is to copy/paste it from etherscan and import
+const daiABI = require('../contractABI/dai.json')
+// Or, ethers knows ERC20 contracts have the same interfaces/functions. So can just the ones you want/need as an ARRAY
+const ERC20_ABI = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+    'function totalSupply() view returns (uint256)',
+    'function balanceOf(address) view returns (uint)',
+]
+
+// ethers.Contract(contractAddress, abi, signerOrProvider)
+const daiContract = new ethers.Contract(daiAddress, ERC20_ABI, provider)
+
+const checkContract = async () => {
+    const name = await daiContract.name()
+    const symbol = await daiContract.symbol()
+    const totalSupply = await daiContract.totalSupply()
+    const balanceOf = await daiContract.balanceOf('0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8')
+
+    console.log(name)
+    console.log(symbol)
+    console.log(ethers.utils.formatUnits(totalSupply, 18)) //18 decimals need to be added
+    console.log(balanceOf.toString())
+}
+checkContract()
